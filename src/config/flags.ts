ba027b9ceade5451969498905083c74a98176eb1
mkarangekis <mkarangekis@augmentationcg.onmicrosoft.com>
2026-02-03 01:37:35 -0500

    Hard enable enterprise UI

--- src/config/flags.ts
+++ src/config/flags.ts
@@ -1,4 +1,1 @@
-const rawEnterpriseFlag =
-  process.env.NEXT_PUBLIC_ENTERPRISE_UI ?? process.env.ENTERPRISE_UI ?? "";
-
-export const isEnterpriseUIEnabled = () => rawEnterpriseFlag === "true";
+export const isEnterpriseUIEnabled = () => true;
